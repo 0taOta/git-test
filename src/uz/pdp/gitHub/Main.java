faafbc667f35abd4c3e26db0675a4bc709c1d8cd
0taOta <150152112+0taOta@users.noreply.github.com>
2023-11-09 20:18:12 +0500

    Update Main.java
    V 0.3

--- src/uz/pdp/gitHub/Main.java
+++ src/uz/pdp/gitHub/Main.java
@@ -3,5 +3,6 @@ package uz.pdp.gitHub;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+         System.out.println("Hello world!");
     }
 }
